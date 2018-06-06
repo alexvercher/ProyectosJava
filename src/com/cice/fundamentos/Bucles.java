3ac4ea387b68502ee529c26ce8161f196c2ce6aa
AVECH3H <email@email.com>
2018-06-06 13:46:17 +0200

    Subida ejercicio 5

--- src/com/cice/fundamentos/Bucles.java
+++ src/com/cice/fundamentos/Bucles.java
@@ -4,16 +4,31 @@ public class Bucles {
 
     public static void main(String[] args) {
 
-        //Bucle FOR
+        //BUCLE FOR
         // for (inicialización; condicion; incremento)
         /*for (int i=0;i<10;i++){
             System.out.println("Contador: " + i);
         }*/
 
-        int[] ArrayEnteros = {1,2,3,4,5,6,7,8,9};
-        //Bucle FOREACH
+        //BUCLE FOREACH
+        /*int[] ArrayEnteros = {1,2,3,4,5,6,7,8,9};
         for (int a: ArrayEnteros){
             System.out.println(a);
+        }*/
+
+        //BUCLE WHILE / DO WHILE
+        int i = 0;
+        int j = 0;
+        int[] ArrayEnteros = {1,2,3,4,5,6,7,8,9};
+
+        while (i<ArrayEnteros.length){
+            System.out.println("I vale: " + i++);
+            //i++;
         }
+
+        do {
+            System.out.println("J vale: " + j);
+            j++;
+        } while (j<ArrayEnteros.length);
     }
 }
